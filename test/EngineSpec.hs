d1bac74442cc64f82e607869cd5f62c07f20890b
Steve Nekoliczak <steve.nekoliczak@protonmail.com>
2023-09-18 23:47:29 -0500

    Add empty spec file for Engine

---
+++ test/EngineSpec.hs
@@ -0,0 +1,10 @@
+module EngineSpec (spec) where
+
+import Test.Hspec
+import Engine
+
+spec :: Spec
+spec = do
+  describe "emptyBoard" $ do
+    it "returns a board with no buildings and no workers" $ do
+      True
