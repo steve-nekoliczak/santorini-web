983ff2b62c12569d292228e95bb5c1f027d9ac59
Steve Nekoliczak <steve.nekoliczak@protonmail.com>
2023-09-20 15:02:58 -0500

    Rework Level to use Enum instead of custom typeclass

--- src/Engine.hs
+++ src/Engine.hs
@@ -25,21 +25,7 @@ data XCoord = XA | XB | XC | XD | XE deriving (Show, Eq, Ord, Enum)
 data YCoord = Y1 | Y2 | Y3 | Y4 | Y5 deriving (Show, Eq, Ord, Enum)
 data Position = NotOnBoard | Position (XCoord, YCoord) deriving (Show, Eq, Ord)
 
-data Top = Top
-class Stackable a where
-  step :: a -> Either Top a
-  curr :: a -> Either Top a
-
-data Level = Ground | LevelOne | LevelTwo | LevelThree | Dome deriving (Show, Eq)
-
-instance Stackable Level where
-  step Ground = Right LevelOne
-  step LevelOne = Right LevelTwo
-  step LevelTwo = Right LevelThree
-  step LevelThree = Right Dome
-  step Dome = Left Top
-  curr Dome = Left Top
-  curr level = Right level
+data Level = Ground | LevelOne | LevelTwo | LevelThree | Dome deriving (Show, Eq, Ord, Enum, Bounded)
 
 data Worker = BlueMan | BlueWoman | IvoryMan | IvoryWoman deriving (Show, Eq, Ord)
 data MaybeWorker w = NoWorker | JustWorker w deriving (Show, Eq, Ord)
@@ -64,12 +50,12 @@ buildUp :: Board -> Position -> Either BuildError Board
 buildUp board targetPosition =
   let targetSpace = spaceOnBoard board targetPosition
   in case targetSpace.worker of
-    JustWorker _            -> Left $ BuildError "Can't build where a worker is"
+    JustWorker _        -> Left $ BuildError "Can't build where a worker is"
     NoWorker            ->
-      case step $ targetSpace.level of
-        Left Top        -> Left $ BuildError "Can't build on top of a dome"
-        Right newLevel  -> Right $ board { grid = updatedGrid }
-          where updatedGrid = insert targetPosition (targetSpace { level = newLevel }) board.grid
+      case targetSpace.level of
+        Dome            -> Left $ BuildError "Can't build on top of a dome"
+        targetLevel     -> Right $ board { grid = updatedGrid }
+          where updatedGrid = insert targetPosition (targetSpace { level = succ targetLevel }) board.grid
 
 moveWorker :: Board -> Worker -> Position -> Either MoveError Board
 moveWorker board workerToMove targetPosition =
@@ -79,9 +65,9 @@ moveWorker board workerToMove targetPosition =
   in case targetSpace.worker of
     JustWorker _        -> Left $ MoveError "Can't move where a worker is"
     NoWorker            ->
-      case curr $ targetSpace.level of
-        Left Top        -> Left $ MoveError "Can't move on top of a dome"
-        Right _         -> Right $ board { grid = updatedGrid, workers = updatedWorkers }
+      case targetSpace.level of
+        Dome            -> Left $ MoveError "Can't move on top of a dome"
+        _               -> Right $ board { grid = updatedGrid, workers = updatedWorkers }
           where updatedGrid = insertMany [updatedOriginSpace, updatedTargetSpace] board.grid
                 updatedWorkers = insert workerToMove targetPosition board.workers
                 updatedOriginSpace = (originPosition, originSpace { worker = NoWorker })
